b0cb7c2e9b9fb1fbf1d1e38b032aef8c13ca51e0
PatrickCmd <pwalukagga@gmail.com>
2018-01-21 21:09:28 +0300

    [Feature #154315451] add providers and store to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import promise from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Navbar from './components/universal/navbar';
+import Carousel from './components/universal/carousel';
+import Homepage from './components/homepage';
+import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
+
+const createStoreWithMiddleware = applyMiddleware(promise, reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        {/* <App /> */}
+        <Router>
+            <div>
+                <Navbar />
+                <Carousel />
+                <Switch>
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/signin" component={Signin} />
+                </Switch>
+            </div>
+        </Router>
+    </Provider>, 
+    document.querySelector('#root')
+);
 registerServiceWorker();
