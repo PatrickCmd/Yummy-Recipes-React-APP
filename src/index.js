710da3d9f8f6faf1234b331a457bbd9751c9a067
PatrickCmd <pwalukagga@gmail.com>
2018-01-29 23:45:42 +0300

    [Feature #154315451] delete irrelevant  files

--- src/index.js
+++ src/index.js
@@ -5,33 +5,54 @@ import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt from 'jsonwebtoken';
+import logger from 'redux-logger'
 
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
-import Navbar from './components/universal/navbar';
-import Carousel from './components/universal/carousel';
+import NavbarComponent from './components/universal/navbar';
 import Homepage from './components/homepage';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
+import SignOut from './components/auth/signOut';
+import Dashboard from './components/universal/dashboard';
+import requireAuth from './components/auth/require_auth';
+import noRequireAuth from './components/auth/no_require_auth';
+import { AUTHENTICATED } from './actions';
 
-const createStoreWithMiddleware = applyMiddleware(promise, reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const createStoreWithMiddleware = applyMiddleware(promise, reduxThunk,logger)(createStore);
+const store = createStoreWithMiddleware(
+    reducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const user = localStorage.getItem('current_user');
+
+if(user) {
+    store.dispatch({ type: AUTHENTICATED, user: jwt.decode(user) });
+}
 
 ReactDOM.render(
     <Provider store={store}>
-        {/* <App /> */}
         <Router>
             <div>
-                <Navbar />
-                <Carousel />
+                <NavbarComponent />
+                <main role="main">
                 <Switch>
                     <Route exact path="/" component={Homepage} />
-                    <Route path="/signup" component={Signup} />
-                    <Route path="/signin" component={Signin} />
+                    <Route path="/dashboard" component={requireAuth(Dashboard)} />
+                    <Route path="/signup" component={noRequireAuth(Signup)} />
+                    <Route path="/signin" component={noRequireAuth(Signin)} />
+                    <Route path="/signout" component={requireAuth(SignOut)} />
                 </Switch>
+                {/* footer */}
+                <footer className="container">
+                    <p>&copy; 2017 - 2018 Company, Inc &middot; Rights Reserved &middot; Designed By &middot; WALUKAGGA PATRICK</p>
+                    <p className="float-right"><a href="#">Back to Top</a></p>
+                </footer>
+                </main>
             </div>
         </Router>
     </Provider>, 
