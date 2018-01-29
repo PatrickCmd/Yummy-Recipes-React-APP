2566ede1d9dc3d657bc9d50119c8d7ec31ee2704
PatrickCmd <pwalukagga@gmail.com>
2018-01-29 23:20:36 +0300

    [Feature #154315451] implement signing in of users

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, FormFeedback, Label, Input} from 'reactstrap';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
+import CarouselComponent from '../universal/carousel';
 import { signInAction } from '../../actions/index';
 
 class Signin extends Component {
 
     // method for rendering form field on the component
     renderField(field) {
+        //de-structuring from the field object
+        const { meta: { touched, error } } = field;
+        const isValid = touched && error ? false : '';
         return (
             <FormGroup>
                 <Label htmlFor={ field.htmlfor }>{ field.label }</Label>
@@ -16,14 +21,15 @@ class Signin extends Component {
                     type={ field.type }
                     id={ field.id }
                     { ...field.input }
+                    valid={ isValid }
                 />
+                <FormFeedback>{ touched ? error : '' }</FormFeedback>
             </FormGroup>
         );
     }
 
     // method callback on form submission
     onSubmitForm = (values) => {
-        console.log(values);
         this.props.signInAction(values, this.props.history);
     }
 
@@ -42,28 +48,53 @@ class Signin extends Component {
         const { handleSubmit } = this.props;
         return (
             <div>
-                <Form onSubmit={ handleSubmit(this.onSubmitForm.bind(this)) }>
-                    <Field 
-                        label="Email"
-                        name="email"
-                        htmlfor="email"
-                        id="email"
-                        type="email"
-                        component={ this.renderField }
-                    />
-                    <Field 
-                        label="Password"
-                        name="password"
-                        htmlfor="password"
-                        type="password"
-                        id="password"
-                        component={ this.renderField }
-                    />
-                    <Button type="submit" color="primary">Sign in</Button>
-                </Form>
+                <CarouselComponent />
+                <div className="container">
+                    <div className="row justify-content-md-center">
+                        <div className="col-md-6">
+                            <h3>SIGN IN</h3>
+                            <hr />
+                            <Form onSubmit={ handleSubmit(this.onSubmitForm.bind(this)) }>
+                                <Field 
+                                    label="Email"
+                                    name="email"
+                                    htmlfor="email"
+                                    id="email"
+                                    type="email"
+                                    component={ this.renderField }
+                                />
+                                <Field 
+                                    label="Password"
+                                    name="password"
+                                    htmlfor="password"
+                                    type="password"
+                                    id="password"
+                                    component={ this.renderField }
+                                />
+                                <Button type="submit" color="primary">Sign in</Button>
+                            </Form>
+                            <Link to="/signup">DON'T HAVE AN ACCOUNT? SIGN UP <i class="fa fa-arrow-right"></i></Link>
+                        </div>
+                    </div>
+                    <hr className="featurette-divider" />
+                </div>
             </div>
         );
     }
+}
+
+// validate input form field from values
+const validate = (values) => {
+    const errors = {};
+    if (!values.email){
+        errors.email = 'Email is required!';
+    }
+
+    if (!values.password) {
+        errors.password = 'Enter your password please!'
+    }
+
+    return errors
 }
 
 const mapStateToProps = (state) => {
@@ -71,6 +102,7 @@ const mapStateToProps = (state) => {
 }
 
 const reduxFormSignin = reduxForm({
+    validate, //validate: validate
     form: 'Signin'
 })(Signin);
 
