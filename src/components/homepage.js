908762d19b4f48205845296560a87bacc52bec92
PatrickCmd <pwalukagga@gmail.com>
2018-01-29 23:24:13 +0300

    [Feature #154315451] add home page component

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import CarouselComponent from '../components/universal/carousel';
 
 class Homepage extends Component {
 
     render() {
         return (
             <div>
-                Home page
+                <CarouselComponent />
+                <div className="container mid-section">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <h3>YUMMY RECIPES</h3>
+                            <hr />
+                            <p>
+                                Whether you love to cook or you just love to eat, odds are you have a collection 
+                                of dishes and recipes you'd like to try. Maybe you have a bunch handed down from a 
+                                loved one. In either case, you probably need a better method to keep them organized 
+                                for the long haul than a bunch of index cards in a file folder.
+                            </p>
+                                Here is Yummy Recipe App for you, depending on the kind of recipe collection you have.
+                            <p>
+                            </p>
+                        </div>
+                    </div>
+
+                    <hr className="featurette-divider" />
+
+                </div> 
+            {/* <!-- Container --> */}
             </div>
         );
     }
