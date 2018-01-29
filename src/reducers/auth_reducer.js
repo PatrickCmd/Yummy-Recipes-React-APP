ba0f22000a0bf6d28b22a21c7563d64a177c0a2b
PatrickCmd <pwalukagga@gmail.com>
2018-01-29 23:27:01 +0300

    [Feature #154315451] implement reducer to return user auth state

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -3,11 +3,12 @@ import { AUTHENTICATED, UNAUTHENTICATED, AUTHENTICATED_ERROR } from '../actions'
 export default (state={}, action) => {
     switch(action.type) {
         case AUTHENTICATED:
-            return { ...state, authenticated: true };
+            return { ...state, authenticated: true, user: action.user };
         case UNAUTHENTICATED:
-            return {  ...state,  authenticated: false };
+            return {  ...state,  authenticated: false, user: {} };
         case AUTHENTICATED_ERROR:
             return { ...state, error: action.payload };
+        default:
+            return state;
     }
-    return state;
 }
