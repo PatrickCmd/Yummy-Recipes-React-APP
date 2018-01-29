5c681383673d2711c3d1de6a997949863ec01a5d
PatrickCmd <pwalukagga@gmail.com>
2018-01-29 23:29:58 +0300

    [Feature #154315451] implement signin, signout, signup action creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './auth_reducer';
+import categoryReducer from './category_reducer';
+import recipeReducer from './recipes_reducer';
 
 /* reducers for the entire project */
 const rootReducer = combineReducers({
     form: formReducer,
-    auth: authReducer
+    auth: authReducer,
+    categories: categoryReducer,
+    recipes: recipeReducer
 });
 
 export default rootReducer;
