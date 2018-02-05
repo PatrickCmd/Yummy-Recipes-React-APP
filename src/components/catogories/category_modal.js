e3c4fe98a560099a20688d15deb5e2e56e7241e0
PatrickCmd <pwalukagga@gmail.com>
2018-02-05 17:28:41 +0300

    [Chore #154315516] Change import of createCategory

--- src/components/catogories/category_modal.js
+++ src/components/catogories/category_modal.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { createCategory } from '../../actions';
+import { createCategory } from '../../actions/categories';
 
 class CategoryModal extends Component {
 
