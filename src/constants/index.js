ec924e1217f4ac0c7b63db94f1ef45c47152df2b
PatrickCmd <pwalukagga@gmail.com>
2018-02-05 17:33:34 +0300

    [Chore #154315516] Put constants in one place

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,15 @@
 export const ROOT_URL = 'http://localhost:5000';
 // export const ROOT_URL = 'https://yummy-recipe-api.herokuapp.com';
+
+export const AUTHENTICATED = 'authenticated_user';
+export const UNAUTHENTICATED = 'unauthenticated_user';
+export const AUTHENTICATED_ERROR = 'authenticated_error';
+export const REGISTER = 'register_user';
+export const USER_ALREADY_EXISTS = 'user_already_exists';
+export const FETCH_CATEGORIES = 'fetch_categories';
+export const FETCH_CATEGORY = 'fetch_category';
+export const FETCH_CATEGORY_RECIPES = 'fetch_category_recipes';
+export const FETCH_RECIPES = 'fetch_recipes';
+export const FETCH_RECIPE = 'fetch_recipe';
+export const CREATE_CATEGORY = 'create_category';
+export const CREATE_RECIPE = 'create_recipe';
