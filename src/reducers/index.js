aa27754516cb97ed7c358bcefa01022814033099
PatrickCmd <pwalukagga@gmail.com>
2018-02-05 09:19:51 +0300

    [Chore #154315516] Add reducers for single category and recipe

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,15 +2,19 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './auth_reducer';
+import categoriesReducer from './categories_reducer';
 import categoryReducer from './category_reducer';
-import recipeReducer from './recipes_reducer';
+import recipesReducer from './recipes_reducer';
+import recipeReducer from './recipe_reducer';
 
 /* reducers for the entire project */
 const rootReducer = combineReducers({
     form: formReducer,
     auth: authReducer,
-    categories: categoryReducer,
-    recipes: recipeReducer
+    categories: categoriesReducer,
+    category: categoryReducer,
+    recipes: recipesReducer,
+    recipe: recipeReducer
 });
 
 export default rootReducer;
