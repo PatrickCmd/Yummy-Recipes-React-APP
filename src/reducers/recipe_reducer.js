b5909dfde3ad3c1cd2c9dd53b3650e8269c4c8e8
PatrickCmd <pwalukagga@gmail.com>
2018-02-05 09:56:51 +0300

    [Chore #154315516] Add reducer to view single recipe

---
+++ src/reducers/recipe_reducer.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+import { FETCH_RECIPE, UNAUTHENTICATED } from '../actions';
+
+export default (state = {}, action) => {
+    switch(action.type) {
+        case "FETCHING":
+            return null
+        case FETCH_RECIPE:
+            return { ...state, recipe_item: action.payload.data.recipe};
+        case UNAUTHENTICATED:
+            return {  ...state,  recipe: {} };
+        default:
+            return state
+    }
+}
