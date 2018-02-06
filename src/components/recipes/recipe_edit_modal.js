ad539f9fb54035b3926f667e0732286f33134472
PatrickCmd <pwalukagga@gmail.com>
2018-02-06 09:41:22 +0300

    [Feature #154315659] Implement editing of recipe item

--- src/components/recipes/recipe_edit_modal.js
+++ src/components/recipes/recipe_edit_modal.js
@@ -3,89 +3,58 @@ import { Field, reduxForm } from 'redux-form';
 import {Col, Button, Form, FormGroup, FormFeedback, FormText, Label, Input } from 'reactstrap';
 import { connect } from 'react-redux';
 
-//import { fetchRecipe } from '../../actions';
+import { editRecipe } from '../../actions/recipes';
 
 class RecipeEditModal extends Component {
 
-    // method for rendering form field on the component
-    renderField(field) {
-        //de-structuring from the field object
-        const { meta: { touched, error } } = field;
-        const isValid = touched && error ? false : '';
-        return (
-            <FormGroup row>
-                <Label htmlFor={ field.htmlfor } md={3}>{ field.label }</Label>
-                <Col md={9}>
-                    <FormText color="muted">
-                        { field.text }
-                    </FormText>
-                    <Input 
-                        type={ field.type }
-                        id={ field.id }
-                        value={ field.value }
-                        { ...field.input }
-                        valid = { isValid }
-                    />
-                    <FormFeedback>{ touched ? error : '' }</FormFeedback>
-                </Col>
-            </FormGroup>
-        );
-    }
-
-    // method callback on form submission
-    onSubmitForm = (values) => {
+    // submiting form values
+    _submit = (values) => {
         const { recipe } = this.props;
-        // this.props.createRecipe(values, category.id, () => {
-        //     window.location.reload();
-        //     this.props.history.push(`/categories/${category.id}`);
-        // });
+        this.props.editRecipe(values, recipe.cat_id, recipe.id, () => {
+            window.location.reload();
+            this.props.history.push(`/categories/${recipe.cat_id}/recipes/${recipe.id}`);
+        });
+    }
+    // handle form submition
+    _onhandleSubmit = (event) => {
+        event.preventDefault();
+        const formData = {};
+        for (const field in this.refs) {
+            formData[field] = this.refs[field].value;
+        }
+        this._submit(formData);
     }
 
     renderModalForm() {
-        const { handleSubmit } = this.props;
         const { recipe } = this.props;
         return (
             <div>
-                <Form onSubmit={ handleSubmit(this.onSubmitForm.bind(this)) } id="recipeForm">
-                    <Field 
-                        label="Recipe name"
-                        name="name"
-                        htmlfor="recipe_name"
-                        id="recipe_name"
-                        type="text"
-                        value={ recipe.name }
-                        component={ this.renderField }
-                    />
-                    <Field 
-                        label="Description"
-                        name="description"
-                        htmlfor="description"
-                        id="description"
-                        type="textarea"
-                        value={ recipe.description }
-                        component={ this.renderField }
-                    />
-                    <Field 
-                        label="Ingredients"
-                        name="ingredients"
-                        htmlfor="ingredients"
-                        id="iingredients"
-                        type="textarea"
-                        value={ recipe.ingredients }
-                        text="Type with space and each on new line."
-                        component={ this.renderField }
-                    />
-                    <Field 
-                        label="Directions"
-                        name="directions"
-                        htmlfor="directions"
-                        id="directions"
-                        type="textarea"
-                        value={ recipe.directions }
-                        text="Type with space and each on new line."
-                        component={ this.renderField }
-                    />
-                </Form>
+                <form id="recipeForm" onSubmit={ this._onhandleSubmit }>
+                <div className="form-group row">
+                    <label for="name" className="col-sm-3 col-form-label" name="name">Name</label>
+                    <div className="col-sm-9">
+                        <input ref="name" type="text" className="form-control" value={ recipe.name } id="name" placeholder="Recipe name" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="description" className="col-sm-3 col-form-label">Description</label>
+                    <div className="col-sm-9">
+                        <textarea ref="description" className="form-control" id="description" name="description">{ recipe.description }</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="ingredients" className="col-sm-3 col-form-label">Ingredients</label>
+                    <div className="col-sm-9">
+                        <textarea ref="ingredients" className="form-control" id="ingredients" name="ingredients">{ recipe.ingredients }</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="directions" className="col-sm-3 col-form-label">Directions</label>
+                    <div className="col-sm-9">
+                        <textarea ref="directions" className="form-control" id="directions" name="directions">{ recipe.directions }</textarea>
+                    </div>
+                </div>
+            </form>
             </div>
         );
     }
@@ -107,7 +76,7 @@ class RecipeEditModal extends Component {
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button type="submit" className="btn btn-success" form="recipeForm">Save</button>
+                            <button type="submit" className="btn btn-info" form="recipeForm">Edit</button>
                         </div>
                         </div>
                     </div>
@@ -117,35 +86,9 @@ class RecipeEditModal extends Component {
     }
 }
 
-// validate input form field from values
-const validate = (values) => {
-    const errors = {};
-    if (!values.recipe_name){
-        errors.recipe_name = 'Name is required!';
-    }
-
-    if (!values.description) {
-        errors.description = 'Type Description!'
-    }
-
-    if (!values.ingredients) {
-        errors.ingredients = 'Recipe must have ingredients'
-    }
-
-    if (!values.directions) {
-        errors.directions = 'Directions must be filled!'
-    }
-
-    return errors
-}
 
 const mapStateToProps = (state) => {
     return { recipe: state.recipe.recipe_item };
 }
 
-export default reduxForm({
-    validate,
-    form: 'RecipeEditForm'
-})(
-    connect(mapStateToProps, { })(RecipeEditModal)
-);
+export default connect(mapStateToProps, { editRecipe })(RecipeEditModal);
