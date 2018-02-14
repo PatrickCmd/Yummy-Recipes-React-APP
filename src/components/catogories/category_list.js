f831de3004d00af007d10c9351a8d6993512c8a0
PatrickCmd <pwalukagga@gmail.com>
2018-02-14 08:56:38 +0300

    [Feature #154315793] Add pagination links

--- src/components/catogories/category_list.js
+++ src/components/catogories/category_list.js
@@ -29,12 +29,56 @@ class CategoriesList extends Component {
         });
     }
 
+    getPaginatedItems = (e) => {
+        e.preventDefault();
+        this.props.fetchCategories(e.target.dataset.page);
+    }
+
+    renderPagination = () => {
+        const { pagination } = this.props;
+        if(!pagination){
+            return (
+                <div>Loading pagination...</div>
+            );
+        }
+        const prevClass = `${pagination.previous_page ? 'page-item' : 'page-item disabled'}`;
+        const nextClass = `${pagination.next_page ? 'page-item' : 'page-item disabled'}`;
+        return (
+            <ul class="pagination">
+                <li className={ prevClass }>
+                    <a 
+                        className="page-link" href="#" tabindex="-1" 
+                        onClick = { this.getPaginatedItems } 
+                        data-page={ pagination.previous_page }>Previous
+                    </a>
+                </li>
+                <li className='page-item'>
+                    <a className="page-link" href="#">
+                        Page { pagination.current_page } of { pagination.pages }
+                    </a>
+                </li>
+                <li className={ nextClass }>
+                    <a 
+                        className="page-link" href="#" 
+                        onClick = { this.getPaginatedItems } 
+                        data-page={ pagination.next_page }>Next
+                    </a>
+                </li>
+            </ul>
+        );
+    }
+
     render() {
         return (
             <div>
                 <ul className="list-group">
                     { this.renderCategories() }
-                </ul>
+                </ul><br />
+                {/* pagination */}
+                <nav aria-label="...">
+                    { this.renderPagination() }
+                </nav>
+
                 <Notifications />
             </div>
         );
@@ -42,7 +86,10 @@ class CategoriesList extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return { categories: state.categories }
+    return { 
+        categories: state.categories.categories,
+        pagination: state.categories.categoryPagination
+    }
 }
 
 export default connect(mapStateToProps, { fetchCategories })(CategoriesList);
