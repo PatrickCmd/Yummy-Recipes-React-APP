6b4b9d4a012b3a11e4e481d05d9fc087e9d3026d
PatrickCmd <pwalukagga@gmail.com>
2018-02-18 11:53:45 +0300

    [Chore #154315871] Add mock data for testing

---
+++ src/__mocks__/mockData.js
@@ -0,0 +1,21 @@
+const mockData = {
+    signInData: {
+        email: "pwalukagga@gmail.com",
+        password: "pato1234"
+    },
+    authResponse: {
+        data: {
+            "auth_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTg2MTQzNzUsImlhdCI6MTUxODUyNzk3NSwidXNlcl9pZCI6MywiZW1haWwiOiJwd2FsdWthZ2dhQGdtYWlsLmNvbSIsImZpcnN0X25hbWUiOiJQYXRyaWNrIiwibGFzdF9uYW1lIjoiV2FsdWthZ2dhIiwicHVibGljX2lkIjoiNzNjMzk2MWUtZWE1YS00ZDBjLTlmYjgtNjdiMmU3ZGQyMzhhIn0._v1tjLCmJAL15w-vryXOlu1RkwEzN3Jsx82To5jr-Ek",
+            "message": "Successfully logged in",
+            "status": "success"
+        }
+    },
+    signUpData: {
+        first_name: "Patrick",
+        last_name: "Walukagga",
+        email: "pwalukagga@gmail.com",
+        password: "pato1234"
+    }
+}
+
+export default mockData;
